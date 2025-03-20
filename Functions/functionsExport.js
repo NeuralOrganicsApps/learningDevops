2a43d8f19255747cfb0a3f6a914cf39418b82dcf
NeuralOrganicsApps <neuralproductionapps@gmail.com>
2025-03-20 23:09:26 +0530

    added arithmatic function

--- Functions/functionsExport.js
+++ Functions/functionsExport.js
@@ -1,6 +1,18 @@
 function add(x,y){
     return x+y
 }
+function sub(x,y){
+    return x-y
+}
+function mul(x,y){
+    return x*y
+}
+function div(x,y){
+    return x/y
+}
 module.exports = {
-    Addition:add
+    Addition:add,
+    Subtraction:sub,
+    Multiplication:mul,
+    Division:div
 }
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const { Addition } = require("./Functions/functionsExport")
+const { Addition, Subtraction, Multiplication, Division } = require("./Functions/functionsExport")
 
 console.log("hello devops")
 console.log("Addition = >" ,Addition(2,3))
+console.log("Subtraction = >" ,Subtraction(2,3))
+console.log("Multiplication = >" ,Multiplication(2,3))
+console.log("Division = >" ,Division(2,3))
